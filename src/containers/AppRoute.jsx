05c6f1f329c08272e1055373b508ba1c7b141f89
Yuval Saraf <yuvalsaraf@gmail.com>
2015-09-16 16:10:03 +0300

    Updated dependencies

--- src/containers/AppRoute.jsx
+++ src/containers/AppRoute.jsx
@@ -1,7 +1,6 @@
 import React, { Component, PropTypes } from 'react';
-import Router, {Route, Redirect} from 'react-router';
-import { history } from 'react-router/lib/BrowserHistory';
-
+import Router, { Route, Redirect } from 'react-router';
+import { createHistory } from 'history';
 import Login from './Login.jsx';
 import TodoApp from './TodoApp.jsx';
 
@@ -17,16 +16,18 @@ export default class AppRoute extends Component {
   }
   render() {
     let component;
+
     if (this.props.isLoggedIn) {
       component = (
-        <Router history={history}>
-      	  <Route path="/" component={TodoApp}/>
-      	  <Redirect from="*" to="/"/>
+        <Router history={createHistory()}>
+      	  <Route path="/main" component={TodoApp}/>
+          <Route path="/login" component={Login}/>
+      	  <Redirect from="*" to="/main"/>
         </Router>
       );
     } else {
       component = (
-        <Router history={history}>
+        <Router history={createHistory()}>
       	  <Route path="/login" component={Login}/>
           <Redirect from="*" to="/login"/>
         </Router>

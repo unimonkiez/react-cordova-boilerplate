ff46752c4b4a573cce933dd2f74dc31182c9112d
Yuval Saraf <yuvalsaraf@gmail.com>
2017-01-19 15:09:01 +0200

    Readded client file

---
+++ src/entry-points/client.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from 'src/containers/app.jsx';
+import AppRoute from 'src/containers/app-route.jsx';
+
+ReactDOM.render(
+  <App>
+    <AppRoute />
+  </App>,
+  document.getElementById('app')
+);
